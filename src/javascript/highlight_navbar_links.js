d26279f95fe3389fdc60fd0298f372f0bcd010c7
jrf115 <jrf115@zips.uakron.edu>
2020-06-19 16:38:09 -0400

    Rewrite JQuery Doc Load functions and add highlight JS

--- src/javascript/highlight_navbar_links.js
+++ src/javascript/highlight_navbar_links.js
@@ -1,4 +1,4 @@
 /** Requires jqery-3.3.1.js **/
 $(document).ready(function () {
-    $("#eagle_project").load("html/projects/eagle_project.html");
+
 });
--- src/javascript/load_page_sections.js
+++ src/javascript/load_page_sections.js
@@ -1,5 +1,5 @@
 /** Requires jquery-3.3.1.js **/
-$(function () {
+$(document).ready(function () {
     $("#head").load("html/head.html");
     $("#header").load("html/header.html");
     $("#footer").load("html/footer.html");

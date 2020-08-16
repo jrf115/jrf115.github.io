a91d3b180ad303cc979f2ba5cc61a7b3a852d20b
jrf115 <jrf115@zips.uakron.edu>
2020-08-16 19:34:02 -0400

    Add basic #2
    This commit adds a primitive dynamic links, with the link to the current page being highlighted "gold"
    after the webpage loads in (using a JQuery ready function).

--- src/javascript/highlight_navbar_links.js
+++ src/javascript/highlight_navbar_links.js
@@ -1,4 +1,28 @@
 /** Requires jqery-3.3.1.js **/
-$(document).ready(function () {
-
+// Call to Highlight current page
+$(document).ready (function highlightNavLink () {  
+    var link = document.getElementsByClassName("current_page")[0].id;
+    switch (link) {
+        case "index":
+            document.getElementById("home_link").className = "col nav-link active";
+            document.getElementById("about_link").className = "col nav-link";
+            document.getElementById("projects_link").className = "col nav-link";
+            break;
+        case "about":
+            document.getElementById("home_link").className = "col nav-link";
+            document.getElementById("about_link").className = "col nav-link active";
+            document.getElementById("projects_link").className = "col nav-link";
+            break;
+        case "projects":
+            document.getElementById("home_link").className = "col nav-link";
+            document.getElementById("about_link").className = "col nav-link";
+            document.getElementById("projects_link").className = "col nav-link active";
+            break;
+        default:
+            alert("Warning: Unable to highlight '" + link + "'.\n");
+            break;
+    }
 });
+
+// Tried to use window.onload functionality to wait for the page sections to load in before this
+// script ran, but it was only working sometimes.    Which was frustrating. 
